ef6278e2b9e55755e29ba8f3ca258846c63b4733
Nikhil Tom Jose <133224167+Ryuou02@users.noreply.github.com>
2025-07-09 07:18:23 +0530

    Add files via upload

---
+++ scavenger hunt/function.js
@@ -0,0 +1,18 @@
+function openTab(tabName,elmnt,color) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+	tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablinks.length; i++) {
+	tablinks[i].style.backgroundColor = "";
+    }
+    document.getElementById(tabName).style.display = "block";
+    if(elmnt.style != null) {
+	elmnt.style.backgroundColor = color;
+    }
+}
+
+
+/* How can I keep Google from indexing my website?  */
